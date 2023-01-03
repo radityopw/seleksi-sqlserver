5c8c0be296e5cd56f4dfe7d2a45243d1965cc924
radityo prasetianto wibowo <radityo_pw@is.its.ac.id>
2023-01-03 11:01:58 +0700

    sample data

---
+++ seleksi/seleksi/Stored Procedures/usp_sample_data.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[usp_sample_data]
+AS
+	
+	-- HAPUS DATA DULU
+	TRUNCATE TABLE _peserta_diterima_final;
+	TRUNCATE TABLE _peserta_diterima_matic;
+	TRUNCATE TABLE peserta;
+	TRUNCATE TABLE penempatan;
+
+	
+	INSERT INTO penempatan(kode_penempatan,nama,daya_tampung)
+	SELECT '1','1',20
+	UNION ALL 
+	SELECT '2','2',21
+	UNION ALL
+	SELECT '3','3',19;
